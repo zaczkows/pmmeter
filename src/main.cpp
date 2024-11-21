66ca6ef600275940e75b69c8a6782aaf07a4fa55
Piotr Zaczkowski <pzaczkowski@protonmail.com>
2024-11-21 11:36:19 +0100

    Minor code improvement

--- src/main.cpp
+++ src/main.cpp
@@ -223,52 +223,19 @@ void setup() {
 void loop() {
     PM25_AQI_Data data;
 
-    if (!aqi.read(&data)) {
-        Serial.println("Could not read from AQI");
-    }
-
     if (WiFi.isConnected()) {
         mqtt_client.loop();
     }
 
-    mqtt_client.publish_pms(data);
+    if (aqi.read(&data)) {
+        LCD_Display_Val(&data);
+        TempHumRead();
+        mqtt_client.publish_pms(data);
+    } else {
+        Serial.println("Could not read from AQI");
+    }
 
-    LCD_Display_Val(&data);
-    TempHumRead();
     printWiFiInformation();
-
-    /*Serial.println();
-    Serial.println(F("---------------------------------------"));
-    Serial.println(F("Concentration Units (standard)"));
-    Serial.println(F("---------------------------------------"));
-    Serial.print(F("PM 1.0: "));
-    Serial.print(data.pm10_standard);
-    Serial.print(F("\t\tPM 2.5: "));
-    Serial.print(data.pm25_standard);
-    Serial.print(F("\t\tPM 10: "));
-    Serial.println(data.pm100_standard);
-    Serial.println(F("Concentration Units (environmental)"));
-    Serial.println(F("---------------------------------------"));
-    Serial.print(F("PM 1.0: "));
-    Serial.print(data.pm10_env);
-    Serial.print(F("\t\tPM 2.5: "));
-    Serial.print(data.pm25_env);
-    Serial.print(F("\t\tPM 10: "));
-    Serial.println(data.pm100_env);
-    Serial.println(F("---------------------------------------"));
-    Serial.print(F("Particles > 0.3um / 0.1L air:"));
-    Serial.println(data.particles_03um);
-    Serial.print(F("Particles > 0.5um / 0.1L air:"));
-    Serial.println(data.particles_05um);
-    Serial.print(F("Particles > 1.0um / 0.1L air:"));
-    Serial.println(data.particles_10um);
-    Serial.print(F("Particles > 2.5um / 0.1L air:"));
-    Serial.println(data.particles_25um);
-    Serial.print(F("Particles > 5.0um / 0.1L air:"));
-    Serial.println(data.particles_50um);
-    Serial.print(F("Particles > 10 um / 0.1L air:"));
-    Serial.println(data.particles_100um);
-    Serial.println(F("---------------------------------------"));*/
 
     delay(1000);
 }
